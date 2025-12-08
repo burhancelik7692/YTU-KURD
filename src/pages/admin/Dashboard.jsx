eeb725873f51570deeca21c2208fc544e73468a3
burhancelik7692 <burhancelik7692@gmail.com>
2025-12-08 10:44:17 +0300

    Vercel Rollup hatasi cozuldu ve vite konfigurasyonu guncellendi

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../../context/AuthContext'; // YOL DÜZELTİLDİ
 import { useNavigate } from 'react-router-dom';
-import { db } from '../../../firebase'; // Bu yol doğru
+import { db } from '../../firebase'; // Bu yol doğru
 import { addDynamicContent } from '../../../services/adminService';
 import { LogOut, Image, Plus, CheckCircle, Loader2, BookOpen, MessageSquare, Book, Trash2, Link as LinkIcon, Edit, AlertCircle, Music, Film } from 'lucide-react';
 import { useLanguage } from '../../../context/LanguageContext'; // YOL DÜZELTİLDİ
